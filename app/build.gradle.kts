0669c3b614d3ee2ad15405e4c799c91b34656c53
Martin Adones <130100685+Pancitox@users.noreply.github.com>
2025-05-28 15:51:00 -0400

    gradle ready

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,10 +48,13 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.core.ktx)
+    implementation(libs.mediation.test.suite)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
 
 }
