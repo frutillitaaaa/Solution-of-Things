2bfad6d3db05390747719fd179f5600feabd74c6
Martin Adones <130100685+Pancitox@users.noreply.github.com>
2025-06-18 15:11:19 -0400

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,11 +6,6 @@ plugins {
 android {
     namespace = "com.example.myapplication"
     compileSdk = 35
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
 
     viewBinding {
         enable = true
@@ -21,7 +16,7 @@ android {
     defaultConfig {
         applicationId = "com.example.myapplication"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 35 // Android 14 (API 35)
         versionCode = 1
         versionName = "1.0"
 
@@ -37,24 +32,28 @@ android {
             )
         }
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
     kotlinOptions {
         jvmTarget = "11"
     }
-    buildToolsVersion = "34.0.0"
 }
 
 dependencies {
+    //Mqtt service
+    implementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.5")
+    //implementation ("org.eclipse.paho:org.eclipse.paho.android.service:1.1.1")
+    implementation("com.android.support:support-v4:28.0.0")
+    implementation ("androidx.work:work-runtime-ktx:2.7.1")
+    implementation ("androidx.core:core-ktx:1.10.1")
+
 
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.core.ktx)
-    implementation(libs.mediation.test.suite)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation("com.google.code.gson:gson:2.10.1")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-
-
 }
